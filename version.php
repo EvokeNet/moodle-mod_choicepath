b1f710ed1210c179213dabcc58afee34183fd5d4
Willian Mano <willianmanoaraujo@gmail.com>
2024-05-25 23:52:01 -0300

    Increase version num. Show alert of choice.

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_choicepath';
-$plugin->release = '0.1.0';
+$plugin->release = '1.0.0';
 $plugin->version = 2024052500;
 $plugin->requires = 2022112800;
 $plugin->maturity = MATURITY_STABLE;
